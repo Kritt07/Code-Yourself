e634cd1524bef2354ce69f2ec5e9e32b7f53456b
kritt <dum98415@gmail.com>
2026-05-05 19:05:34 +0500

    добавил список существующих команд

--- View/Screens/MainMenuScreen.cs
+++ View/Screens/MainMenuScreen.cs
@@ -69,6 +69,46 @@ namespace CodeYourself.View.Screens
             play.Click += (_, __) => PlayRequested?.Invoke(this, EventArgs.Empty);
             buttons.Controls.Add(play);
 
+            var commands = new NeonButton(_theme)
+            {
+                Text = "КОМАНДЫ",
+                Width = 280,
+                Height = 54,
+                Margin = new Padding(0, 0, 0, 14)
+            };
+            commands.Click += (_, __) =>
+            {
+                var help =
+                    "Доступные команды:\r\n" +
+                    "\r\n" +
+                    "MOVE LEFT|RIGHT [n]\r\n" +
+                    "  - шаг на 1 клетку (если n не задан — 1)\r\n" +
+                    "\r\n" +
+                    "JUMP LEFT|RIGHT n\r\n" +
+                    "  - прыжок на n клеток (n = 1..3)\r\n" +
+                    "\r\n" +
+                    "WAIT [n]\r\n" +
+                    "  - подождать n тиков (если n не задан — 1)\r\n" +
+                    "\r\n" +
+                    "REPEAT n\r\n" +
+                    "  <команды>\r\n" +
+                    "END\r\n" +
+                    "  - повторить блок n раз\r\n" +
+                    "\r\n" +
+                    "Пример:\r\n" +
+                    "REPEAT 3\r\n" +
+                    "  MOVE RIGHT 1\r\n" +
+                    "  JUMP RIGHT 1\r\n" +
+                    "END\r\n";
+
+                var owner = FindForm();
+                if (owner != null)
+                    MessageBox.Show(owner, help, "Список команд", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show(help, "Список команд", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            };
+            buttons.Controls.Add(commands);
+
             var exit = new NeonButton(_theme)
             {
                 Text = "ВЫХОД",
